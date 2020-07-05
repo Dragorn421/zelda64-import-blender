8e202e1fc32c861ce5eac20891d3d3c9fe9d741e
Dragorn421 <m.arthur@orange.fr>
2020-07-05 11:48:55 +0200

    bl_info version 2.3

--- io_import_z64.py
+++ io_import_z64.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name":        "Zelda64 Importer",
-    "version":     (2, 2),
+    "version":     (2, 3),
     "author":      "SoulofDeity",
     "blender":     (2, 6, 0),
     "location":    "File > Import-Export",
